cd67813b3eda5c9d5316725dc0664f76a30bf74c
shivam9753 <shivam9753@gmail.com>
2025-08-05 19:38:02 +0530

    Changed Prod API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://13-204-38-128.nip.io/api'  // Production API endpoint using nip.io
+  apiBaseUrl: 'https://app.poemsindia.in/api'  // Production API endpoint using nip.io
 };
